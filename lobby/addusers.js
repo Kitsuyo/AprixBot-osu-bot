00890238b0a7e70f535ca6e7b60eda75a659f5fb
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-08 13:00:26 +0200

    bug fix

--- lobby/addusers.js
+++ lobby/addusers.js
@@ -2,7 +2,7 @@ const {writeJson} = require(`fs-extra`);
 
 module.exports = {
     async code(scores) {
-        let uniqueUsers = require(`../uniqueUsers.json`);
+        let uniqueUsers = require(`./uniqueUsers.json`);
         scores.forEach(score => {
             if (uniqueUsers.usernames.includes(score.player.user.ircUsername)) return;
             uniqueUsers.count+=1;
